dd20d2ce96aa1378088b0a32dffdefa5b95cb172
mukesh <pothanamukesh@gmail.com>
2025-01-08 06:11:03 -0600

    push all changes to git

---
+++ src/main/java/com/springframework/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.springframework.microservices.currencyconversionservice;
+
+import com.springframework.microservices.currencyconversionservice.entity.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name ="currency-exchange",url = "localhost:8000")
+@FeignClient(name ="currency-exchange-service")
+public interface CurrencyExchangeProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion currencyExchange(@PathVariable String from, @PathVariable String to);
+}
---
+++ src/main/java/com/springframework/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -0,0 +1,47 @@
+package com.springframework.microservices.currencyconversionservice.controller;
+
+import com.springframework.microservices.currencyconversionservice.CurrencyExchangeProxy;
+import com.springframework.microservices.currencyconversionservice.entity.CurrencyConversion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+public class CurrencyConversionController {
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
+                                                           @PathVariable String to,
+                                                           @PathVariable BigDecimal quantity) {
+        HashMap<String,String > uriVariables = new HashMap<>();
+        uriVariables.put("from",from);
+        uriVariables.put("to",to);
+
+        ResponseEntity<CurrencyConversion> responsEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responsEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(),from,to,
+                quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+    }
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
+                                                           @PathVariable String to,
+                                                           @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion currencyConversion = currencyExchangeProxy.currencyExchange(from, to);
+        return new CurrencyConversion(currencyConversion.getId(),from,to,
+                quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+    }
+}
